82a4af51c2310bce6fc10a7eb84f47d96685e071
huxiubin <huxiubin@wakedata.com>
2024-11-13 11:13:07 +0800

    chore: 动画性能优化

--- components/layout/sections/empty.tsx
+++ components/layout/sections/empty.tsx
@@ -3,10 +3,12 @@ import { cn } from "@/lib/utils";
 
 export const EmptySection = () => {
   return (
-    <section id="empty" className="h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+    <section
+      id="empty"
+      className="h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg"
+    >
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-
-      {/* <Boxes /> */}
+      <Boxes />
       <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
         Building...
       </h1>
