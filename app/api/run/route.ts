a5cbabeda3ec2872a2dcb75166f5f31f918dcfae
huxiubin <huxiubin@wakedata.com>
2024-12-13 09:55:33 +0800

    fix: 修改线上接口缓存问题

--- app/api/run/route.ts
+++ app/api/run/route.ts
@@ -3,7 +3,9 @@ import { findRunData } from "@/server/controller/run";
 
 export async function GET(req: NextRequest) {
   try {
-    return NextResponse.json({ rData: await findRunData() });
+    const response =  NextResponse.json({ rData: await findRunData() });
+    response.headers.set('Cache-Control', 'no-cache');
+    return response;
   } catch (e) {
     return NextResponse.json({ error: "failed" }, { status: 500 });
   }
